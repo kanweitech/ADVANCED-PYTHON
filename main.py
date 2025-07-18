8cc3fc37c2e2fe58fd0a7417bc1e8cf10da9dd3f
kanweitech <68553986+kanweitech@users.noreply.github.com>
2025-07-18 13:48:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+def get_sample_log_data():
+  #generates sample log data for demonstration.
+  return [
+    {"timestamp":"2025-07-18 12:00", "message":"Server started"},
+    {"timestamp":"2025-07-18 12:01", "message":"User logged in"},
+    #add more sample log
+  ]
+
+def get_sample_genome_data():
+  #provides sample DNA sequences for demonstration.
+  return ["AGGCTA","CGTAAC","TTAGGC"]
+
+def get_sample_stock_data():
+  #sample stock prices for demonstration.
+  return [
+    {"day":"Monday", "price":150},
+    {"day":"Tuesday", "price":155},
+    {"day":"Wednesday", "price":145},
+    #Add more sample stock data
+  ]
+   
